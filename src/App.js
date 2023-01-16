2515e44c2c948fd231c64e381d85cf7c2a823bdb
devv-anderson <anderson.barros96@outlook.com>
2023-01-16 16:20:52 -0300

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Pagination from 'react-paginate';
+import axios from 'axios';
+import styles from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [characters, setCharacters] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const response = await axios.get(`https://rickandmortyapi.com/api/character?name=${name}&page=${currentPage + 1}`);
+                setCharacters(response.data.results);
+                setTotalPages(response.data.info.pages);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, [currentPage, name]);
+
+    const handlePageChange = (selectedPage) => {
+        setCurrentPage(selectedPage.selected);
+    }
+
+    return (
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <input placeholder='Type a character name' type="text" value={name} onChange={e => setName(e.target.value)} />
+          </header>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+              <div>
+                    <div className={styles.container}>
+                        {characters.map((character) => (
+                            <div key={character.id}>
+                                <img src={character.image} alt={character.name}/>
+                                <div className={styles.info}>
+                                <p>Name: {character.name}</p>
+                                <p>Status: {character.status}</p>
+                                <p>Specie: {character.species}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                     <Pagination
+                     containerClassName={styles.pagination}
+                     pageCount={totalPages}
+                     forcePage={currentPage}
+                     onPageChange={handlePageChange}
+                    />
+              </div>
+                )}
+        </div>
+    )
 }
 
 export default App;
